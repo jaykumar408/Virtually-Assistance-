a30808c14aff93460062a7eaf2d83a53e1e32f7a
jaykumar408 <147745478+jaykumar408@users.noreply.github.com>
2025-03-03 14:00:11 +0530

    Add files via upload

---
+++ 032522_8.Virtual_Assistant/8.Virtual Assistant/script.js
@@ -0,0 +1,98 @@
+let btn=document.querySelector("#btn")
+let content=document.querySelector("#content")
+let voice=document.querySelector("#voice")
+
+function speak(text){
+    let text_speak=new SpeechSynthesisUtterance(text)
+    text_speak.rate=1
+    text_speak.pitch=1
+    text_speak.volume=1
+    text_speak.lang="hi-GB"
+    window.speechSynthesis.speak(text_speak)
+}
+
+function wishMe(){
+    let day=new Date()
+    let hours=day.getHours()
+    if(hours>=0 && hours<12){
+        speak("Good Morning Sir")
+    }
+    else if(hours>=12 && hours <16){
+        speak("Good afternoon Sir")
+    }else{
+        speak("Good Evening Sir")
+    }
+}
+// window.addEventListener('load',()=>{
+//     wishMe()
+// })
+let speechRecognition= window.SpeechRecognition || window.webkitSpeechRecognition 
+let recognition =new speechRecognition()
+recognition.onresult=(event)=>{
+    let currentIndex=event.resultIndex
+    let transcript=event.results[currentIndex][0].transcript
+    content.innerText=transcript
+   takeCommand(transcript.toLowerCase())
+}
+
+btn.addEventListener("click",()=>{
+    recognition.start()
+    voice.style.display="block"
+    btn.style.display="none"
+})
+function takeCommand(message){
+   voice.style.display="none"
+    btn.style.display="flex"
+    if(message.includes("hello")||message.includes("hey")){
+        speak("hello sir,what can i help you?")
+    }
+    else if(message.includes("who are you")){
+        speak("i am SunnyFunds  virtual assistant ,created by Jay Kumar Sir")
+      if(message.includes("Hi! I want to check if I’m eligible for a loan.")||message.includes("hey")){
+            speak( "Sure! To determine your eligibility, I need a few details. Can you tell me your credit score, monthly income, and the loan amount you need?")
+        }
+        if(message.includes("My credit score is 680, my monthly income is 4,500, and I want to borrow 20,000.")||message.includes("hey")){
+            speak(  "Let me check... (Processing) Based on your credit score and income, you are eligible for a loan of up to $25,000. Would you like me to guide you through the loan application process?")
+        }
+    }else if(message.includes("open youtube")){
+        speak("opening youtube...")
+        window.open("https://youtube.com/","_blank")
+    }
+    else if(message.includes("open google")){
+        speak("opening google...")
+        window.open("https://google.com/","_blank")
+    }
+    else if(message.includes("open facebook")){
+        speak("opening facebook...")
+        window.open("https://facebook.com/","_blank")
+    }
+    else if(message.includes("open instagram")){
+        speak("opening instagram...")
+        window.open("https://instagram.com/","_blank")
+    }
+    else if(message.includes("open calculator")){
+        speak("opening calculator..")
+        window.open("calculator://")
+    }
+    else if(message.includes("open whatsapp")){
+        speak("opening whatsapp..")
+        window.open("whatsapp://")
+    }
+else if(message.includes("My credit score is 680, my monthly income is $4,500, and I want to borrow $20,000.")){
+    speak("opening youtube...")
+    window.open("https://youtube.com/","_blank")
+}
+    else if(message.includes("time")){
+      let time=new Date().toLocaleString(undefined,{hour:"numeric",minute:"numeric"})
+      speak(time)
+    }
+    else if(message.includes("date")){
+        let date=new Date().toLocaleString(undefined,{day:"numeric",month:"short"})
+        speak(date)
+      }
+    else{
+        let finalText="this is what i found on internet regarding" + message.replace("shipra","") || message.replace("shifra","")
+        speak(finalText)
+        window.open(`https://www.google.com/search?q=${message.replace("SunnyFunds ","")}`,"_blank")
+    }
+}
